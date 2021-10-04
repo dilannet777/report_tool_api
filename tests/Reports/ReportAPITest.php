ff684ed2b1dcdd83e63577927095833e828e38e9
dilan <dilan@cleato.com>
2021-10-04 19:41:24 +0530

    added more test cases

--- tests/Reports/ReportAPITest.php
+++ tests/Reports/ReportAPITest.php
@@ -4,11 +4,6 @@ namespace Tests\Reports;
 
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Exception\RequestException;
 use PHPUnit\Framework\TestCase;
 
 
@@ -16,15 +11,21 @@ class ReportAPITest extends TestCase
 {
     
     protected $client;
+
+    protected function setUp(): void
+    {
+        $this->client = new Client([
+            'base_uri' => getenv('TEST_API_URL'),
+            'exceptions' => false
+        ]);
+    }
+
     // tested getTurnOverReport()
 
     public function testTurnoverDailyReport()
     {
         
-        $this->client = new Client([
-            'base_uri' => 'http://127.0.0.1:8000',
-            'exceptions' => false
-        ]);
+        
         $response = $this->client->request('POST','/api/reports/turnover',[
             'headers' => [ 'Accept' => 'application/json'],
             'json'=>['type' => 'daily']]);
@@ -35,10 +36,6 @@ class ReportAPITest extends TestCase
     public function testTurnoverBrandReport()
     {
         
-        $this->client = new Client([
-            'base_uri' => 'http://127.0.0.1:8000',
-            'exceptions' => false
-        ]);
         $response = $this->client->request('POST','/api/reports/turnover',[
             'headers' => [ 'Accept' => 'application/json'],
             'json'=>['type' => 'brand']]);
